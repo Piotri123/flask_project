89a59f74611eb7d1645746b5d54af90eac12f22f
Piotri123 <pehostak@gmail.com>
2023-09-15 22:32:25 +0000

    Adds BootstrapCSS

--- mdblog/app.py
+++ mdblog/app.py
@@ -6,38 +6,36 @@ from .mod_main import main
 from .mod_blog import blog
 from .mod_admin import admin
 
+
 import os
 
 flask_app = Flask(__name__)
 flask_app.config.from_pyfile("/vagrant/configs/default.py")
-
 if "MDBLOG_CONFIG" in os.environ:
     flask_app.config.from_envvar("MDBLOG_CONFIG")
-
 db.init_app(flask_app)
 
 flask_app.register_blueprint(main)
 flask_app.register_blueprint(blog)
-flask_app.register_blueprint(admin)
+flask_app.register_blueprint(admin, url_prefix="/admin")
 
 @flask_app.errorhandler(500)
 def internal_server_error(error):
-    return render_template("errors/500.jinja")
+    return render_template("errors/500.jinja"), 500
 
 @flask_app.errorhandler(404)
 def internal_server_error(error):
-    return render_template("errors/404.jinja")
+    return render_template("errors/404.jinja"), 404
 
-#region CLI COMMAND
+## CLI COMMAND
 def init_db(app):
     with app.app_context():
         db.create_all()
         print("database inicialized")
 
-        defualt_user = User(username="admin")
-        defualt_user.set_password("admin")
+        default_user = User(username="admin")
+        default_user.set_password("admin")
 
-        db.session.add(defualt_user)
+        db.session.add(default_user)
         db.session.commit()
         print("default user was created")
-#endregion
--- mdblog/mod_blog/controller.py
+++ mdblog/mod_blog/controller.py
@@ -9,9 +9,11 @@ blog = Blueprint("blog", __name__)
 
 @blog.route("/articles/", methods=["GET"])
 def view_articles():
-    page = request.args.get("page", 1 , type=int)
+    page = request.args.get("page", 1, type=int)
     paginate = Article.query.order_by(Article.id.desc()).paginate(page, 5, False)
-    return render_template("mod_blog/articles.jinja", articles=paginate.items, paginate=paginate)
+    return render_template("mod_blog/articles.jinja",
+            articles=paginate.items,
+            paginate=paginate)
 
 @blog.route("/articles/<int:art_id>/")
 def view_article(art_id):
